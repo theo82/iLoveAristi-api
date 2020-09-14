aa505367ce53d07ef626e82f253ca3fb3c57e87e
theo82 <theoaristi53@gmail.com>
2020-09-14 08:34:43 +0300

    Make some changes

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ const riverRouter = require('./routes/riverRouter');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
